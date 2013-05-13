382e4581330242acf586b9d572d7f185cdd40700
Josephinebrown <jojobrown@fullsail.edu>
2013-05-13 11:59:10 -1000

    recap on things i did

--- Arithmetic Operators/js/script.js
+++ Arithmetic Operators/js/script.js
@@ -1,7 +1,10 @@
-// Expression Arithemitic Operators
+// Expression Arithemitic Operators Order of Operation PEMDAS
 
-//area of the triangle is half of the width time the height
-var width = 4;
-var height = 5;
-var area = width * height;
-console.log(area);
+var quiz1 = 87;
+var quiz2 = 100;
+var quiz3 = 60;
+var quiz4 = 80;
+
+//the average is the sum of the items divided by the number of items
+var average = quiz1 + quiz2 + quiz3 + quiz4;
+consol.log(average);
