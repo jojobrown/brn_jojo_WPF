262cfcf45f3a64dc3537fe4ed1e9b628ab7988d0
Josephinebrown <jojobrown@fullsail.edu>
2013-05-13 23:39:42 -1000

    changed what we are looking for

--- Expression_Indusrty/js/script.js
+++ Expression_Indusrty/js/script.js
@@ -2,6 +2,8 @@
 //May 16, 2013
 //Expression_Industry
 
-//Booking appointments for Account Excutive on a weekly average, than do a monthly average
+//Booking appointments for AE, need to find out weekly average than monthly average
+
+
 
 
