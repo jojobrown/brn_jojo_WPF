feee1ab33153f5b5daaddb6b593a51a4323a03b4
Josephinebrown <jojobrown@fullsail.edu>
2013-05-14 13:09:18 -1000

    inputted my variable on what I know

--- Expression_Wacky/js/script.js
+++ Expression_Wacky/js/script.js
@@ -5,4 +5,4 @@
 
 //Baby bags of balls she has 8 bags of ball but they all have different amounts of balls in them. Find out how many ball altogether and devide them up evenly amoung the bags.
 
-
+var ballBags = [10, 8, 6, 12, 9, 7, 4, 14]
