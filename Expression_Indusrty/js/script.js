8280c78e68792734e841ed50faf2274323cba29b
Josephinebrown <jojobrown@fullsail.edu>
2013-05-14 11:10:18 -1000

    Put in the "The daily average appointment made in the console log that that it would appear in the console log.

--- Expression_Indusrty/js/script.js
+++ Expression_Indusrty/js/script.js
@@ -26,7 +26,7 @@ var day19 = 4;
 var day20 = 20;
 
 var averageAppointment = (day1 + day2 + day3 + day4 + day5 + day6 + day7 + day8 + day9 + day10 + day11 + day12 + day13 + day14 + day15 + day16 + day17 + day18 + day19 + day20)/20;
-console.log(averageAppointment) 
+console.log("The daily averrage appointment that was made"+  +averageAppointment) 
 
 
 
