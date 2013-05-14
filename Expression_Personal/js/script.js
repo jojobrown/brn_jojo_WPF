d0545cb24e1c4105cd4eb4375b7148d3555270c5
Josephinebrown <jojobrown@fullsail.edu>
2013-05-13 20:59:15 -1000

    added in an alert

--- Expression_Personal/js/script.js
+++ Expression_Personal/js/script.js
@@ -6,14 +6,16 @@
 
 //got it the first time nowon to used
 
-var c = ("how many bags used");
-var c = 3  * 7  ;
+var bags = ("how many bags used");
+var bags = 3  * 7  ;
  
 var bags = prompt("Trying to figure out how many bags we will use if you use 3 bags an hour and work for 7 hours. \nPlease enter bags per hour")
 
 var hours = prompt("Please enter hours worked")
 
-console.log(c);
+//console.log(c);
+
+alert("How many bags used");
 
 
 
