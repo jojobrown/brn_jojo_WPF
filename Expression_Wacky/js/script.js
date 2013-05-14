4701ff66f274b88a9f61d42da7ba01b86620ee3a
Josephinebrown <jojobrown@fullsail.edu>
2013-05-14 13:10:43 -1000

    added in if there is going to be a remainder. to the problem

--- Expression_Wacky/js/script.js
+++ Expression_Wacky/js/script.js
@@ -3,6 +3,6 @@
 //Expression
 
 
-//Baby bags of balls she has 8 bags of ball but they all have different amounts of balls in them. Find out how many ball altogether and devide them up evenly amoung the bags.
+//Baby bags of balls she has 8 bags of ball but they all have different amounts of balls in them. Find out how many ball altogether and devide them up evenly amoung the bags. If there will a remainder.
 
 var ballBags = [10, 8, 6, 12, 9, 7, 4, 14]
