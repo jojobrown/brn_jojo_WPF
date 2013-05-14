e4e15eac3b991c10f9c423e29e079284e7f5949a
Josephinebrown <jojobrown@fullsail.edu>
2013-05-13 23:52:19 -1000

    added in what is none and changed job descriptions

--- Expression_Indusrty/js/script.js
+++ Expression_Indusrty/js/script.js
@@ -2,8 +2,18 @@
 //May 16, 2013
 //Expression_Industry
 
-//Booking appointments for AE, need to find out weekly average than monthly average
+//Booking appointments for AE, need to find out daily average then weekly average of appointment booked with 5 days making a week
 
+var day1 = 10;
+var day2 = 14;
+var day3 = 8;
+var day4 = 18;
+var day5 = 5;
+var day6 = 9;
+var day7 = 17;
+var day8 = 11;
+var day9 = 1;
+var day10= 6;
 
 
 
