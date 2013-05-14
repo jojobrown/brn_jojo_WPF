4aa0d63f07a543a38df8bc0628e1615be2b4a012
Josephinebrown <jojobrown@fullsail.edu>
2013-05-14 10:53:17 -1000

    had to make sure everything was spelled correctly

--- Expression_Indusrty/js/script.js
+++ Expression_Indusrty/js/script.js
@@ -26,7 +26,7 @@ var day19 = 4;
 var day20 = 20;
 
 var appointmentMade = day1 + day2 + day3 + day4 + day5 + day6 + day7 + day8 + day9 + day10 + day11 + day12 + day13 + day14 + day15 + day16 + day17 + day18 + day19 + day20;
-console.log(appointMade) 
+console.log(appointmentMade) 
 
 
 
