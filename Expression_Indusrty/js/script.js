c5ec4555323118488b4b946633b9e81b44de9d93
Josephinebrown <jojobrown@fullsail.edu>
2013-05-14 00:14:05 -1000

    added more days

--- Expression_Indusrty/js/script.js
+++ Expression_Indusrty/js/script.js
@@ -2,7 +2,7 @@
 //May 16, 2013
 //Expression_Industry
 
-//Booking appointments for AE, need to find out daily average then weekly average of appointment booked with 5 days making a week
+//Booking appointments for AE, need to find out monthly average booked with 5 days making a week
 
 var day1 = 10;
 var day2 = 14;
@@ -13,7 +13,18 @@ var day6 = 9;
 var day7 = 17;
 var day8 = 11;
 var day9 = 1;
-var day10= 6;
+var day10 = 6;
+var day11 = 20;
+var day12 = 16;
+var day13 = 3;
+var day14 = 14;
+var day15 = 8;
+var day16 = 0;
+var day17 = 11;
+var day18 = 7;
+var day19 = 4;
+var day20 = 20;
+
 
 
 
