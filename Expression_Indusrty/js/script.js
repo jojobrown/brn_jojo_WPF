6fbb60b7217b670cdfd7ca7fe45d3c160acd13cb
Josephinebrown <jojobrown@fullsail.edu>
2013-05-14 11:04:26 -1000

    instead of monthly average looking for daily average of appointments booked

--- Expression_Indusrty/js/script.js
+++ Expression_Indusrty/js/script.js
@@ -2,7 +2,7 @@
 //May 16, 2013
 //Expression_Industry
 
-//Booking appointments for AE, need to find out monthly average booked with 5 days making a week
+//Booking appointments for AE, need to find out daily average of appointments booked with 5 days making a week
 
 var day1 = 10;
 var day2 = 14;
@@ -25,7 +25,7 @@ var day18 = 7;
 var day19 = 4;
 var day20 = 20;
 
-var averageAppointment = day1 + day2 + day3 + day4 + day5 + day6 + day7 + day8 + day9 + day10 + day11 + day12 + day13 + day14 + day15 + day16 + day17 + day18 + day19 + day20;
+var averageAppointment = (day1 + day2 + day3 + day4 + day5 + day6 + day7 + day8 + day9 + day10 + day11 + day12 + day13 + day14 + day15 + day16 + day17 + day18 + day19 + day20)/20;
 console.log(averageAppointment) 
 
 
