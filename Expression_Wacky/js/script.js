f96d0af12e8c3576b5b9e1e6db6e2fed93174a3d
Josephinebrown <jojobrown@fullsail.edu>
2013-05-14 13:05:32 -1000

    staging the problem and what needs to be found

--- Expression_Wacky/js/script.js
+++ Expression_Wacky/js/script.js
@@ -1,3 +1,8 @@
 // Josephine Brown
 //May 16, 2012
 //Expression
+
+
+//Baby bags of balls she has 8 bags of ball but they all have different amounts of balls in them. Find out how many ball altogether and devide them up evenly amoung the bags.
+
+
