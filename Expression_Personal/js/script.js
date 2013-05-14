bbc78511bb0ae5f19c47ad47af398312ee361886
Josephinebrown <jojobrown@fullsail.edu>
2013-05-13 20:11:36 -1000

    got it the first time

--- Expression_Personal/js/script.js
+++ Expression_Personal/js/script.js
@@ -4,6 +4,12 @@
 
 //cleaning yard uses 3 bags an hour for trash, worked for 7 hours. How many bags did we use for the day.
 
+//got it the first time nowon to used
+
 var c = ("how many bags used");
-var c = 3 * 7;
+var c = 3  * 7  ;
 console.log(c);
+
+
+
+
