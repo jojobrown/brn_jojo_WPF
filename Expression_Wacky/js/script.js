0fddedd252e65d494b60c9bd58274f4f18da514e
Josephinebrown <jojobrown@fullsail.edu>
2013-05-14 16:31:02 -1000

    added in the percent sign to get the remainder to show

--- Expression_Wacky/js/script.js
+++ Expression_Wacky/js/script.js
@@ -3,11 +3,11 @@
 //Expression
 
 
-//Baby bags of balls she has 8 bags of ball but they all have different amounts of balls in them. Find out how many ball altogether and devide them up evenly amoung the bags. If there will a remainder.
+//Baby bags of balls she has 8 bags of ball but they all have different amounts of balls in them. Find out how many ball altogether and devide them up evenly amoung the bags. If there will a remainder.  So we want to find out the remainder
 
 var ballBags = [10, 8, 6, 12, 9, 7, 4, 14]
 
-var total = (ballBags[0] + ballBags[1] + ballBags[2] + ballBags[3] + ballBags[4] + ballBags[5] +ballBags[6] + ballBags[7])/8
+var total = (ballBags[0] + ballBags[1] + ballBags[2] + ballBags[3] + ballBags[4] + ballBags[5] +ballBags[6] + ballBags[7])%8
 
 console.log(total)
 
