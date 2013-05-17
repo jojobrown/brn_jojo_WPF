8dbd3faa1ee295f1ba8337d82d455b75be2b214b
Josephinebrown <jojobrown@fullsail.edu>
2013-05-16 16:27:47 -1000

    changed the whole situation to  an "else if" situation

--- Conditional Logic/js/script.js
+++ Conditional Logic/js/script.js
@@ -1,15 +1,19 @@
 //Conditional Logic - else if
 
-var kidHeight = 45;
+var kidHeight = 46;
 var minHeight = 48;
+var wParentHeight = 45;//the height of the kid with the parent present
 
 //if the child is old enough, print in the console "you can ride!"
 //if kid is over 48 inches in height
 if(kidHeight > minHeight){
-	//code performed if condition is true
+	//you can ride!
 	console.log("You can ride the coaster!")
-}else{
-	//code performed it condtion is false
-	console.log("Sorry kid, you've got some growing to do first!");
+}else if(kidHeight > wParentHeight ){
+	//you can ride with a parentpresent
+	console.log("You can ride, but only with a parent present");
 
+}else{
+	//sorry you have a growing to do
+	console.log("Sorry kid, you've got some growing to do first!");
 }
