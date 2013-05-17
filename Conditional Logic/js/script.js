7b7d5a8d528cec044969b5894726cca116e5a0b8
Josephinebrown <jojobrown@fullsail.edu>
2013-05-16 16:05:27 -1000

    added in = sign to the second conditional to see what happens

--- Conditional Logic/js/script.js
+++ Conditional Logic/js/script.js
@@ -9,7 +9,7 @@ if(kidHeight > minHeight){
 	//code performed if condition is true
 	console.log("You can ride the coaster!")
 }
-if(kidHeight < minHeight){
+if(kidHeight <= minHeight){
 	console.log("Sorry kid, you've got some growing to do first!");
 
 }
