7539b3b6c18679a6571474acc15404a4cb841eb6
Josephinebrown <jojobrown@fullsail.edu>
2013-05-16 15:49:33 -1000

    add a variable for sneaker lift and took out plus 2 in the conditional line

--- Conditional Logic/js/script.js
+++ Conditional Logic/js/script.js
@@ -1,11 +1,12 @@
 //Conditional Logic - With an Expressions
 
 var kidHeight = 47;
-var minHeight = 48
+var minHeight = 48;
+var sneakerLift = 2;
 
 //if the child is old enough, print in the console "you can ride!"
 //if kid is over 48 inches in height
-if(kidHeight + 2 > minHeight){
+if(kidHeight + sneakerLift > minHeight){
 	//code performed if condition is true
 	console.log("You can ride the coaster!")
 }
