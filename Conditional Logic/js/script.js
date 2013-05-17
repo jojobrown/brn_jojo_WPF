adbfbdb25fe42de30c361f14cb555a72368902bf
Josephinebrown <jojobrown@fullsail.edu>
2013-05-16 15:32:26 -1000

    changed the variable to a > than sign

--- Conditional Logic/js/script.js
+++ Conditional Logic/js/script.js
@@ -3,7 +3,8 @@
 var kidHeight = 30;
 
 //if the child is old enough, print in the console "you can ride!"
-if(oldEnough){
+//if kid is over 48 inches in height
+if(kidHeight > 48){
 	//code performed if condition is true
 	console.log("You can ride the coaster!")
 }
