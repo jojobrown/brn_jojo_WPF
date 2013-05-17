c8e477898473651e6c78c5dfadf49210e45fe789
Josephinebrown <jojobrown@fullsail.edu>
2013-05-16 15:57:48 -1000

    Took out variable sneakerLift, and the conditional back to kid Height  > minheight, than added in another conditional to see if not that than this

--- Conditional Logic/js/script.js
+++ Conditional Logic/js/script.js
@@ -2,11 +2,14 @@
 
 var kidHeight = 47;
 var minHeight = 48;
-var sneakerLift = 2;
 
 //if the child is old enough, print in the console "you can ride!"
 //if kid is over 48 inches in height
-if(kidHeight + sneakerLift > minHeight){
+if(kidHeight > minHeight){
 	//code performed if condition is true
 	console.log("You can ride the coaster!")
+}
+if(kidHeight < minHeight){
+	console.log("Sorry kid, you've got some growing to do first!");
+
 }
