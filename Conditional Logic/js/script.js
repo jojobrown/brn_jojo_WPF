8bf0356709a654bad4860f53acbc16d851585202
Josephinebrown <jojobrown@fullsail.edu>
2013-05-16 15:37:41 -1000

    added in another variable minheight to see what results

--- Conditional Logic/js/script.js
+++ Conditional Logic/js/script.js
@@ -1,10 +1,11 @@
 //Conditional Logic - Relational Expressions
 
 var kidHeight = 50;
+var minHeight = 48
 
 //if the child is old enough, print in the console "you can ride!"
 //if kid is over 48 inches in height
-if(kidHeight > 48){
+if(kidHeight > minHeight){
 	//code performed if condition is true
 	console.log("You can ride the coaster!")
 }
