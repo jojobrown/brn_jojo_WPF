ba5889b9737a26d461126641a9b7f9133bd2d8d5
Josephinebrown <jojobrown@fullsail.edu>
2013-05-18 23:12:29 -1000

    re phrased the alert to match with the answer to come

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -15,12 +15,13 @@ if(disneyWorld > vacationBudget){
 	console.log("We will be headed to Disney World!!")
 }
 
+
 var winTrip = prompt("Please answer the question. What state is Disney Land in?")
 
 var reAnswer = prompt("Are you sure you picked" + " " + winTrip + "? Please anwer yes or no.")
 
 
-alert(winTrip)
+alert("The answer you chose was"+ " " + winTrip + "! The correct answer is California!")
 console.log("The state you chose was" + " " + winTrip)
 
 
