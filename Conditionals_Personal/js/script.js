fbc3231dfc951ed7580ab12af45d970055d90630
Josephinebrown <jojobrown@fullsail.edu>
2013-05-18 22:20:23 -1000

    took our one var disney land and changed the conditional around to work.

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -6,14 +6,13 @@
 
 var vacationBudget = 5000;
 var disneyWorld = 5895;
-var disneyLand = 5001;
 
 //going to do the equation to see if it works
 
 if(disneyWorld > vacationBudget){
 	console.log("Then we will go to Disney Land!!")
-}else if(disneyLand > vacationBudget){
-	console.log("Then we will stay home and play toourist!!!")
+}else{
+	console.log("We will be headed to Disney World!!")
 }
 
 //change price of Disney Land to offset to see if it works
