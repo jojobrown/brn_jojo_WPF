ad9d909cc33c90a8d08f1bd8ade508bbd200e56d
Josephinebrown <jojobrown@fullsail.edu>
2013-05-18 21:42:17 -1000

    added in my variables

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -3,3 +3,7 @@
 //Conditionals_Personal
 
 //Planning a vacation want to see if we have enough money to go Disney World or Disney Land
+
+var vacationBudget = 5000;
+var disneyWorld = 5895;
+var disneyLand = 4620;
