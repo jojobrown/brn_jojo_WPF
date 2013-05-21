8fb480ee44ba71526d349079fd673cc2a1b953c6
Josephinebrown <jojobrown@fullsail.edu>
2013-05-20 22:03:08 -1000

    next step on what i did and why

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -12,6 +12,7 @@ if(clientCreditScore > creditScoreLoan){
 	//we can get the loan
 	console.log("We got the loan and open our business!!")
 }else if(clientCreditScore > scoreWithCosigner){
+	//we can get the loan but with some help
 	console.log("We still get the loan but with help and a partner!!")
 }else{
 	console.log("We go home and figure something else to do!!")
