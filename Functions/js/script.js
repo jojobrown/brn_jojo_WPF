0e1935c73b58df4055b3ce1e5c776336c7651ec9
Josephinebrown <jojobrown@fullsail.edu>
2013-05-23 13:24:10 -1000

    canceling the first and putting in an new argument

--- Functions/js/script.js
+++ Functions/js/script.js
@@ -1,11 +1,21 @@
 // Function = Parameters and Arguments
 
 //storage bin for info parameter
-function dogYears(age){//parameters
-	var dogYears = age * 7;
-	console.log("Sparky is " + dogYears + " years old.")
-}
+//function dogYears(age){//parameters
+//var dogYears = age * 7;
+//	console.log("Sparky is " + dogYears + " years old.")
+//}
 
 //passing info argument
-dogYears(6);//arguments
-dogYears(8);
+//dogYears(6);//arguments
+//dogYears(8);
+//
+
+//Functions = Returning Values
+
+calcArea(30, 20);
+
+function calcArea(w, h){
+	var area = w * h;
+	console.log(area);
+}
