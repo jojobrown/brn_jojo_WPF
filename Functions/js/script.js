5a0e2f9e1cc3f8c82f069205ae39f6b2e917eec9
Josephinebrown <jojobrown@fullsail.edu>
2013-05-23 13:18:14 -1000

    once again clarifying

--- Functions/js/script.js
+++ Functions/js/script.js
@@ -1,11 +1,11 @@
 // Function = Parameters and Arguments
 
 //storage bin for info parameter
-function dogYears(age){
+function dogYears(age){//parameters
 	var dogYears = age * 7;
 	console.log("Sparky is " + dogYears + " years old.")
 }
 
 //passing info argument
-dogYears(6);
+dogYears(6);//arguments
 dogYears(8);
