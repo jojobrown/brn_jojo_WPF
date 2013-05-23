24e3d2deb2969e5f025e504ce92192906ba5d12c
Josephinebrown <jojobrown@fullsail.edu>
2013-05-23 13:10:03 -1000

    printing it out to console log

--- Functions/js/script.js
+++ Functions/js/script.js
@@ -1,7 +1,11 @@
 // Function = Parameters and Arguments
 
+//storage bin for info
 function dogYears(age){
 	var dogYears = age * 7;
 	console.log("Sparky is " + dogYears + " years old.")
 }
-dogYears(4);
+
+//passing info
+dogYears(6);
+dogYears(8);
