5be4fd5c46c7747d072cf2563b5d76f019d161d5
Josephinebrown <jojobrown@fullsail.edu>
2013-05-27 23:06:29 -1000

    added in console .log to the prompt

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -18,3 +18,4 @@ var nameInitials = prompt("Just to make sure you came to the right website pleas
 var ouncesWater = prompt("To conclude this survery please tell me about how many oucnes of water do you dirnk a day? Please enter it below it ounces")
 //inputting alert
 alert(nameInitials + " " + "like to drink" + " " + ouncesWater + " " + "ounces of water a day.")
+console.log(nameInitials + " " + "like to drink" + " " + ouncesWater + " " + "ounces of water a day.")
