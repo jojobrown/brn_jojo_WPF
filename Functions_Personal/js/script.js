3c0b8dd70f1820a5c401cecb4e3a970ed77200b9
Josephinebrown <jojobrown@fullsail.edu>
2013-05-27 23:10:48 -1000

    rearragned the ounces of milk

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -5,13 +5,13 @@
 // How many ounces of milk does a baby need
 // using months of baby till 12 months
 
-ouncesMilk(5)//argument
-ouncesMilk(8)
-
 function ouncesMilk(babiesAge){//parameter
 	var ounces = babiesAge * 2
 	console.log("The baby drinks" + " " + ounces + " " + "ounces of milk due to her age.")
 }
+
+ouncesMilk(5)//argument
+ouncesMilk(8)
 
 //inputting prompt
 var nameInitials = prompt("Just to make sure you came to the right website please enter you initials in capital letters.")
