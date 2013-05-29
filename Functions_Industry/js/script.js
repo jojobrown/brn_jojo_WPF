c0620dc3aec7137f5c193874fc3e308fdf20f014
Josephinebrown <jojobrown@fullsail.edu>
2013-05-28 23:29:30 -1000

    changed the price from 100 to 300

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -5,7 +5,7 @@
 //client will take a deal depending on what is on the table as far how many spots they can get for certain amount
 
 function clientAccepting(){
-	var fiftySpots = 100
+	var fiftySpots = 300
 	var clientBudget = 200 
 	var clientAccepts = clientBudget > fiftySpots
 	console.log(clientAccepts)	
