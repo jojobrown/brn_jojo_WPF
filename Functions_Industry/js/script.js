b24747c69572ff94ae3d1ad8f8bbb0a45e77cfd8
Josephinebrown <jojobrown@fullsail.edu>
2013-05-28 22:24:19 -1000

    setting up the function

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -3,3 +3,13 @@
 // Functions_Industry
 
 //client will take a deal depending on what is on the table as far as advertising and internet advertising 
+
+function clientAccepting(){
+	if(){
+	
+	}else{
+		
+	}
+	console.log()	
+}
+	
