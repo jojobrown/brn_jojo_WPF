b5588aa1832c27f25f903fa2daee5c4012253dba
Josephinebrown <jojobrown@fullsail.edu>
2013-05-28 22:59:35 -1000

    changed my scenario to make that it will make sense

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -2,10 +2,11 @@
 // May 30,2012
 // Functions_Industry
 
-//client will take a deal depending on what is on the table as far as advertising and internet advertising 
+//client will take a deal depending on what is on the table as far how many spots they can get for certain amount
 
 function clientAccepting(){
-	if(){
+	var fiftySpots = 100 
+	if(fiftySpots){
 	
 	}else{
 		
