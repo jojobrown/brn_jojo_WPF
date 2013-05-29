8deb4921c7613a2c468be399d16b6fa3d922e56e
Josephinebrown <jojobrown@fullsail.edu>
2013-05-28 22:18:38 -1000

    added in name date title and scenario

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -1,1 +1,5 @@
-// JavaScript Document
+// Josephine Brown	
+// May 30,2012
+// Functions_Industry
+
+//client will take a deal depending on what is on the table as far as advertising and internet advertising 
