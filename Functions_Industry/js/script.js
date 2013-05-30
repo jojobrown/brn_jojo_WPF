8ac9f4e45c5fea6ba69f990a68a6539ee7fbc139
Josephinebrown <jojobrown@fullsail.edu>
2013-05-29 20:24:17 -1000

    redid all prompts and added in alerts

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -14,7 +14,11 @@ clientAccepting();
 clientAccepting();
 clientAccepting();
 
-var countingBugs = prompt("There was 1 ladybugs, 2 ants, 3 catipliers and 4 butterflies. Can you please tell me how many flies bugs we have and how many crawling ones i have? You can apply in the box below.")
+var countingBugs = prompt("There was 1 ladybug, 2 ants, 3 catipliers and 4 butterflies. Can you please tell me how many flying bugs we have and how many crawling ones we have? You can apply in the box below. Please labe which ones are the flying ones and which one is the crawling ones.")
+
+var bugsCounting = prompt("The first screen mentioned 10 bugs, do you remember the bugs and how much of each I had. If you do please list how much and which bugs they were.")
+
+alert("You fisrt mentioned that we had" + " " + countingBugs + "." + "Then you remembered we had" + " " + bugsCounting +"." + "Thank you for your input and time.")
 
 	
 
