fc9924a4f9f9bbb7d7ae295b0844713827764142
Josephinebrown <jojobrown@fullsail.edu>
2013-05-29 20:04:36 -1000

    changing dollar amounts for budget and from fifty spots to sixty spots

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -5,13 +5,13 @@
 //client will take a deal depending on what is on the table as far how many spots they can get for certain amount
 
 function clientAccepting(){
-	var spots = 100
-	var clientBudget = 200 
-	var clientAccepts = clientBudget > spots
+	var sixtySpots = 300
+	var clientBudget = 450 
+	var clientAccepts = clientBudget > sixtySpots
 	console.log(clientAccepts)	
 }
 clientAccepting();
 
 var countingBugs = prompt("There was 3 ladybugs, 10 ants, 7 catipliers and 5 butterflies. Can you please tell me how many flies bugs we have and how many crawling ones i have? You can apply in the box below.")	
 
- 
+var  
