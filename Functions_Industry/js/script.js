0131b3091a36265bb59be0883c934eeea9cf19dc
Josephinebrown <jojobrown@fullsail.edu>
2013-05-29 19:57:51 -1000

    just trying to rearrange to make sure to works and it does not seem to be working

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -5,11 +5,13 @@
 //client will take a deal depending on what is on the table as far how many spots they can get for certain amount
 
 function clientAccepting(){
-	var fiftySpots = 300
+	var spots = 100
 	var clientBudget = 200 
-	var clientAccepts = clientBudget > fiftySpots
+	var clientAccepts = clientBudget > spots
 	console.log(clientAccepts)	
 }
 clientAccepting();
 
 var countingBugs = prompt("There was 3 ladybugs, 10 ants, 7 catipliers and 5 butterflies. Can you please tell me how many flies bugs we have and how many crawling ones i have? You can apply in the box below.")	
+
+ 
